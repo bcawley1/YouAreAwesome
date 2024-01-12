694c34cf3718c2ceb3f4be06ca006c172b5a45f9
bcawley1 <braydencawley@gmail.com>
2024-01-12 11:08:59 -0700

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Cawley, Brayden J on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .padding()
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
